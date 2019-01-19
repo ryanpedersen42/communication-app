23b9c88cd0786e72c99aeb768edb6590d55b150b
ryanpedersen42 <ryanpedersen42@gmail.com>
2019-01-18 16:40:54 -0800

    adding about page to start building on

---
+++ src/Components/About/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return(
+    <div>
+    <h1>placeholder for about page</h1>
+      <p>pensive helps teams communicate on their own time.<br/>
+      reduce alert fatigue, improve deep work time, schedule your day your way.
+      </p>
+    </div>
+  )
+}
+
+export default About;
