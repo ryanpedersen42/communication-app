3c298e685b3780ecd0784b97f4a8479023c79e0d
ryanpedersen42 <ryanpedersen42@gmail.com>
2019-02-25 14:19:27 -0800

    other header logo

--- src/Components/Header/LogInHeader.js
+++ src/Components/Header/LogInHeader.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './Header.css';
-import logo from './logo.png';
+// import logo from './logo.png';
 import altlogo from './altlogo.png';
 import { NavLink } from 'react-router-dom';
 
