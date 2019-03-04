0fc845aa0de213435486018bfaf2946113be247e
ryanpedersen42 <ryanpedersen42@gmail.com>
2019-03-03 17:16:17 -0800

    new card

--- src/Components/Cards/Card.js
+++ src/Components/Cards/Card.js
@@ -3,7 +3,6 @@ import './Card.css';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router';
 
-//https://codesandbox.io/s/400lp1yjjw?from-embed this might help with opening box and close button
 class Card extends Component { 
   constructor(props){
     super(props);
@@ -11,26 +10,8 @@ class Card extends Component {
       username: this.props.user.user,
       postSelected: false,
     }
-    this.onFollow = this.onFollow.bind(this);
     this.onUpvote = this.onUpvote.bind(this);
   };
-
-  onFollow = (event) => {
-    fetch('http://localhost:3000/api/follow', {
-      method: 'post',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        username: this.state.username,
-        following: event,
-      })
-    })
-    .then((response) => response.json())
-    .then(response => {
-        // console.log('heres the resp', response)
-      }).catch((err)=>{
-        console.log(err)
-      })
-  }
 
   onUpvote = (event) => {
     fetch('http://localhost:3000/api/upvote', {
@@ -71,7 +52,6 @@ class Card extends Component {
         <div className='pa4'>
           <section className="mw5 mw7-ns center bg-moon-gray pa3 ph3-ns relative" >
             <div className="top-1 right-1 absolute">
-              <button type="button" className="f6 link dim ph3 pv2 dib right mb2 white bg-black" onClick={() => this.onFollow(id)}>Follow</button>
               <button type="button" onClick={this.postSelected} className="f6 link dim ph3 pv2 dib right mb2 white bg-black">Open Up -></button>
             </div>
             <div>
